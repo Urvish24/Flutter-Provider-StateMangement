055367abd0a765064383942b49d61b0cc3ca3db0
Urvish123 <urvish.jani@hexagoninfosoft.com>
2021-07-07 18:40:50 +0530

    Work with multiple provider

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -4,71 +4,77 @@ import 'package:myapp/provider/counterProvider.dart';
 import 'package:myapp/provider/sliderProvider.dart';
 import 'package:provider/provider.dart';
 
-class HomePage extends StatefulWidget{
+class HomePage extends StatefulWidget {
+
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  var _counterprovider;
+  var _Sliderprovider;
+
+  @override
+  void initState() {
+    _counterprovider = Provider.of<counterProvider>(context, listen: false);
+    _Sliderprovider = Provider.of<sliderProvider>(context, listen: false);
+
+    print("provider " + _counterprovider.counter.toString());
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<counterProvider>(
-      create:  (_) => counterProvider(),
-      child: Scaffold(
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Consumer<counterProvider>(
-                builder: (context, provider, _) {
-                  return Column(
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+                   Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      URisedButton(title: 'Increament',onTap: ()=> provider.incrementCounter()),
+                      URisedButton(title: 'Increament',
+                          onTap: () => _counterprovider.incrementCounter()),
                       SizedBox(height: 5,),
                       Row(mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text(provider.counter.toString()),
+                            Consumer<counterProvider>(
+                              builder: (context, provider, _) {
+                                return Text(provider.counter.toString());
+                              }),
                             SizedBox(width: 20,),
-                            ElevatedButton(onPressed: ()=>provider.resetCounter(),child: Text('reset'),)]),
-                            SizedBox(height: 5,),
-                            URisedButton(title: 'Decreament',onTap: ()=> provider.decncrementCounter(),),
+                            ElevatedButton(onPressed: () =>
+                                _counterprovider.resetCounter(), child: Text('reset'),)
+                          ]),
+                      SizedBox(height: 5,),
+                      URisedButton(title: 'Decreament',
+                        onTap: () => _counterprovider.decncrementCounter(),),
 
+                    ],
+            ),
+            Consumer<sliderProvider>(
+                builder: (context, provider, _) {
+                  return Column(
+                    children: [
+                      SizedBox(height: 20,),
+                      Text(provider.getSlidervalue.round().toString()),
+                      Slider(
+                        min: 0,
+                        max: 100,
+                        value: provider.value,
+                        onChanged: (value) {
+                          provider.sliderChange(value);
+                        },
+                      ),
                     ],
                   );
                 }
-              ),
-              Consumer<sliderProvider>(
-                  builder: (context, provider, _) {
-                    return Column(
-                      children: [
-                        SizedBox(height: 20,),
-                        Text(provider.getSlidervalue.round().toString()),
-                        Slider(
-                          min: 0,
-                          max: 100,
-                          value: provider.value,
-                          onChanged: (value) {
-                            provider.sliderChange(value);
-                          },
-                        ),
-                      ],
-                    );
-                  }
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
   }
 
-  @override
-  void initState() {
-    final _store = Provider.of<counterProvider>(context, listen: false);
 
-    print("provider "+_store.counter.toString());
-  }
 }
